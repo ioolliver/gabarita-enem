9deed043afb188f6c80aa9f9e568d296e21df177
ioolliver <101019491+ioolliver@users.noreply.github.com>
2024-10-01 20:10:29 -0300

    fix: portuguese fix

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,12 @@ export default async function Home() {
       </section>
       <section className="flex text-center flex-col gap-2 my-8">
         <h1 className="text-3xl font-black">O que são habilidades?</h1>
-        <h2 className="text-xl font-medium text-justify md:px-16">A prova de Linguagens do ENEM não cobra conteúdos, e sim habilidades. Por isso, a forma correta de estudar para ela não é através de conteúdos tradicionais, mas sim pelas 30 habilidades descritas na Matriz de referência.</h2>
-        <h2 className="text-xl font-medium text-justify md:px-16">Desta forma, o Gabarita Linguagens veio com uma proposta inovadora: gerar listas de exercícios personalizadas de acordo com as habilidades selecionadas. É bem fácil: você seleciona as habilidades que deseja treinar, quantas questões deseja e nós geramos uma lista fresquinha para você. Você responde e pode ver como foi o seu desempenho. Dessa forma, gabaritar Linguagens fica fácil.</h2>
+        <h2 className="text-xl font-medium text-justify md:px-16">A prova de Linguagens do ENEM não cobra conteúdos, mas sim habilidades. Por isso, a forma correta de estudar para ela não é por meio de conteúdos tradicionais, mas sim pelas 30 habilidades descritas na Matriz de Referência.</h2>
+        <h2 className="text-xl font-medium text-justify md:px-16">Dessa forma, o Gabarita Linguagens traz uma proposta inovadora: gerar listas de exercícios personalizadas de acordo com as habilidades selecionadas. É bem simples: você escolhe as habilidades que deseja treinar, define o número de questões e nós geramos uma lista fresquinha para você. Você responde e pode ver como foi o seu desempenho. Assim, fica fácil gabaritar Linguagens.</h2>
       </section>
       <section className="flex text-center flex-col gap-4 my-8">
         <h1 className="text-3xl font-black">O que é o Gabarita Linguagens?</h1>
-        <h2 className="text-xl font-medium text-justify md:px-16">O Gabarita Linguagens é um projeto muito recente. Lançado em outubro de 2024, tem como objetivo quebrar o paradigma conteúdista de ensino quando o assunto é a Prova de Linguagens. O projeto, nesse momento, conta com sua ajuda para se manter de pé. Siga-nos nas redes sociais para saber das últimas atualizações, se possível, doe algum valor.</h2>
+        <h2 className="text-xl font-medium text-justify md:px-16">O Gabarita Linguagens é um projeto muito recente. Lançado em outubro de 2024, seu objetivo é quebrar o paradigma conteúdista do ensino tradicional para a Prova de Linguagens. Neste momento, o projeto conta com a sua ajuda para se manter ativo. Siga-nos nas redes sociais para acompanhar as últimas atualizações e, se possível, faça uma doação.</h2>
         <p className="text-sm">OBS: a previsão é que todas as questões do ENEM de 2023 a 2009 sejam cadastradas até 05/10. Depois disso, as questões de provas PPL serão adicionadas até 01/11 (2 dias antes do ENEM). Siga nosso Instagram para ficar por dentro das atualizações.</p>
         <div className="">
           <a href="https://ko-fi.com/gabaritalinguagens" target="_blank">
