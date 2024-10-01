9bfda76adbc449cbfcfe50869dbe970937781128
ioolliver <101019491+ioolliver@users.noreply.github.com>
2024-10-01 10:16:57 -0300

    feat: change alt image logo

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <header className="w-full border-b-2 flex items-center">
             <a className="flex items-center" href="/">
-                <Image src={Logo} alt="Logo do Gabarita Linguagens. Machado de Assis." className="w-20 p-2" />
+                <Image src={Logo} alt="Machado de Assis. Logo do Gabarita Linguagens" className="w-20 p-2" />
                 <h1 className="font-bold text-xl">Gabarita Linguagens</h1>
             </a>
         </header>
