e6bc290c3d6b1770bda58b10d650f5a53a809eec
ioolliver <a>
2024-10-01 10:15:01 -0300

    fix: logo fix

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <header className="w-full border-b-2 flex items-center">
             <a className="flex items-center" href="/">
-                <Image src={Logo} alt="Logo do Gabarita Linguagens. Machado de Assis." className="w-20" />
+                <Image src={Logo} alt="Logo do Gabarita Linguagens. Machado de Assis." className="w-20 p-2" />
                 <h1 className="font-bold text-xl">Gabarita Linguagens</h1>
             </a>
         </header>
