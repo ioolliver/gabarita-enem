60007eff239bafca71cb39547b8f60eb4ae49b0e
root <root@ioolliver.>
2024-12-31 18:32:24 -0300

    feat: add alert on list creation page

--- app/lists/create/page.tsx
+++ app/lists/create/page.tsx
@@ -4,6 +4,9 @@ export default function Page() {
     return (
         <main>
             <Header />
+            <div>
+                <p>Estamos passando por reformas. Aguarde por novidades!</p>
+            </div>
         </main>
     )
 }
